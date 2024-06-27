dfe715ebc46e1e18235effaa601b7643bbc16c73
Salman <sallunasrullah@gmail.com>
2024-06-27 22:18:51 +0400

    specific response is send

--- src/main/java/com/microservice_ecommerce/gateway/jwt/AuthTokenFilter.java
+++ src/main/java/com/microservice_ecommerce/gateway/jwt/AuthTokenFilter.java
@@ -3,6 +3,7 @@ package com.microservice_ecommerce.gateway.jwt;
 import com.microservice_ecommerce.gateway.clients.IdentityClient;
 import com.microservice_ecommerce.gateway.clients.UserClient;
 import com.microservice_ecommerce.gateway.external.User;
+import feign.FeignException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -10,6 +11,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
@@ -40,6 +42,7 @@ public class AuthTokenFilter extends OncePerRequestFilter {
 
                 if (userId != null) {
                     User user = userClient.getUserById(userId);
+                    logger.debug("Fetched user details: {}", user);
 
                     UsernamePasswordAuthenticationToken authentication =
                             new UsernamePasswordAuthenticationToken(
@@ -49,14 +52,25 @@ public class AuthTokenFilter extends OncePerRequestFilter {
                     authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
 
                     SecurityContextHolder.getContext().setAuthentication(authentication);
+                } else {
+                    logger.warn("User ID returned null for token: {}", token);
+                    response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Invalid token: User ID is null");
+                    return;
                 }
             }
+        } catch (FeignException feignException) {
+            logger.error("FeignException occurred while validating token: {}", feignException.contentUTF8());
+            response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            response.getWriter().write(feignException.contentUTF8());
+            return;
         } catch (Exception exception) {
             logger.error("Cannot set user authentication: {}", exception);
+            response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Cannot set user authentication: " + exception.getMessage());
+            return;
         }
 
         filterChain.doFilter(request, response);
-
     }
 
     private String parseBearerToken(HttpServletRequest request) {
